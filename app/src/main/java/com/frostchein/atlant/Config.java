f34e5d1b6ec8252bdb049d74c35134eaa3baa841
fastcode <fastcode1@mail.ru>
2017-10-31 19:39:32 +0300

    Added ATL token transfer functionality

--- app/src/main/java/com/frostchein/atlant/Config.java
+++ app/src/main/java/com/frostchein/atlant/Config.java
@@ -16,7 +16,7 @@ public class Config {
   //atl
   public static final String TOKEN_ATL_NAME = "ATL";
   public static String TOKEN_ATL_ADDRESS = "0x78B7FADA55A64dD895D8c8c35779DD8b67fA8a05";
-  public static long TOKEN_ATL_ID = 0L;
+  public static long TOKEN_ATL_ID = 2835717307L;
 
   public static int SIZE_PX_QR = 500;
   public static int MAX_TRANSACTIONS = 500;
