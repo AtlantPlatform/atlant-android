f738e0b06c571f4e2249b0a3d0f55d5f113ed6a1
feainnewedd <feainneweddfeainnewedd@gmail.com>
2021-02-25 11:13:06 +0700

    copyright update

--- app/src/main/java/io/atlant/wallet/model/Transactions.java
+++ app/src/main/java/io/atlant/wallet/model/Transactions.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright 2017-2019 Tensigma Ltd. All rights reserved.
+ * Copyright 2017-2021 Digital Asset Exchange Limited. All rights reserved.
  * Use of this source code is governed by Microsoft Reference Source
  * License (MS-RSL) that can be found in the LICENSE file.
  */
